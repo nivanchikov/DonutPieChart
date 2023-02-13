5977d48abcb051a2a393b716f071622da682a29b
Alex Potrivaev <alexpotrivaev@gmail.com>
2023-02-13 10:31:21 +0000

    Initial Commit

---
+++ DonutPieChart/DonutPieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonutPieChartApp.swift
+//  DonutPieChart
+//
+//  Created by Oleksandr Potryvaiev on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DonutPieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
